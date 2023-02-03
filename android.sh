a2e930d5cbac0896cbc1db148bdfbaf7757d5020
Ozuduuuu <124299443+Ozuduuuu@users.noreply.github.com>
2023-02-03 12:14:34 +0400

    Create android.sh

---
+++ android.sh
@@ -0,0 +1,26 @@
+MESAJ="LegendUserBot Otomatik Deploy Kuruluma Hoş Geldiniz"
+MESAJ+="\nTelegram: @legendot"
+pkg update -y
+clear
+echo -e $MESAJ
+echo "Python Yüklənir"
+pkg install python -y
+clear
+echo -e $MESAJ
+echo "Git Yüklənir"
+pkg install git -y
+clear
+echo -e $MESAJ
+echo "TeleThon Yüklənir"
+pip install telethon
+echo "Repo klonlanır..."
+git clone https://github.com/Oxu/installerlegend
+clear
+echo -e $MESAJ
+cd Installer
+clear
+echo "Bəzi alt yapi dosyaları Yüklənir"
+echo -e $MESAJ
+pip install wheel
+pip install -r requirements.txt
+python -m merlin_installer
